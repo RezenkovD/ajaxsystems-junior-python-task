c469a0af532c03500fad56a4057208e7e19d8220
RezenkovD <rezenkovdmitro@gmail.com>
2022-08-12 15:14:28 +0300

    Create TCP client

--- __main__.py
+++ __main__.py
@@ -1,10 +1,35 @@
+import asyncio
 from PySide2 import QtWidgets, QtGui
 
 import server  # DON`T REMOVE
 
+HOST = "localhost"
+PORT = 8888
+MAX_BYTES = 2048
+MAX_CHUNKS = 256
+
+
+async def get_picture():
+    reader, writer = await asyncio.open_connection(HOST, PORT)
+    chuncks = []
+    count = 0
+    while count < MAX_CHUNKS:
+        writer.write(b"next")
+        await writer.drain()
+        chunk = await reader.read(MAX_BYTES)
+        chuncks.append(chunk)
+        if not chunk:
+            break
+        count += 1
+    chuncks.sort()
+    chuncks = [x[1:] for x in chuncks]
+    return b"".join(chuncks)
+
 
 def client():
-    path_to_image = ""
+    path_to_image = "picture"
+    with open(path_to_image, "wb") as file:
+        file.write(asyncio.run(get_picture()))
     return path_to_image
 
 
